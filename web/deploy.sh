000907e690afd757ea6ac00b97a3c055fa824f01
mvincentmiller <m.vincent83@gmail.com>
2018-01-28 17:55:22 -0600

    ...

--- web/deploy.sh
+++
@@ -1,7 +0,0 @@
-# heroku login
-# git config remote.origin.url https://{USERNAME}:{PASSWORD}@github.com/{USERNAME}/{REPONAME}.git
-git add .
-git commit -m $1
-git push origin master
-docker build -t xapi .
-heroku container:push web --app domaintoken
